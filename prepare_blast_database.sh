5fa05c37a6de0f7d71177e6645a498f105af3f89
Patrick Hyden <patrick_hyden@gmx.at>
2018-05-15 16:08:17 +0200

    bug fixed: outdated merged taxids replacement

--- prepare_blast_database.sh
+++ prepare_blast_database.sh
@@ -15,5 +15,7 @@ module load ncbiblastplus
 for enog in $(less $enog_list); do
 	cp bactNOG_raw_algs/*"."$enog"."* $enog".fa"
 	sed 's/\..*$/\t/;s/^>/\t>/;' $enog".fa" | tr -d '\n\-' | tr "\t" "\n" > $target_dir/$enog".fa"
+	#some taxids changed (merged) after eggnog release. sed to replace the headers (so they fit the taxonomy tree)
+	sed -i 's/>245018$/>649756/;s/>439483$/>929558/;s/>469595$/>1639133/;s/>469596$/>100884/;s/>525366$/>1423814/;' $target_dir/$enog".fa"
 	makeblastdb -in $target_dir/$enog".fa" -dbtype prot 
 done
