ac7c8662a5223a398039b6b965ca351f0f3009c0
Patrick Hyden <patrick_hyden@gmx.at>
2018-05-17 09:45:57 +0200

    do not copy database if already existing

--- prepare_blast_database.sh
+++ prepare_blast_database.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
-tmpdir=/tmp/bactNOG
-enog_list=$(pwd)/data/curated_34_enogs.txt
+PATH_TO_BACTNOG_DB=/mirror/eggnog/eggnog_4.5/data/bactNOG/bactNOG.raw_algs.tar.gz
 
-target_dir=$(pwd)/data/databases
-mkdir -p $target_dir $tmpdir
+TMPDIR=/tmp/bactNOG
+ENOG_LIST=$(pwd)/data/curated_34_enogs.txt
 
-cd $tmpdir
+TARGET_DIR=$(pwd)/data/databases
+mkdir -p ${TARGET_DIR} $TMPDIR
 
-tar -xf /mirror/eggnog/eggnog_4.5/data/bactNOG/bactNOG.raw_algs.tar.gz -C .
+cd $TMPDIR
+
+DATABASE_EXISTING=1
+for ENOG in $(less ${ENOG_LIST}); do
+	if ! [ -f ${TARGET_DIR}/$ENOG".fa" ]; then
+		DATABASE_EXISTING=0
+	fi
+done
+
+
+if [ ${DATABASE_EXISTING} -eq 0 ]; then
+	tar -xf ${PATH_TO_BACTNOG_DB} -C .
+fi
 
 module load ncbiblastplus
 
-for enog in $(less $enog_list); do
-	cp bactNOG_raw_algs/*"."$enog"."* $enog".fa"
-	sed 's/\..*$/\t/;s/^>/\t>/;' $enog".fa" | tr -d '\n\-' | tr "\t" "\n" > $target_dir/$enog".fa"
-	#some taxids changed (merged) after eggnog release. sed to replace the headers (so they fit the taxonomy tree)
-	sed -i 's/>245018$/>649756/;s/>439483$/>929558/;s/>469595$/>1639133/;s/>469596$/>100884/;s/>525366$/>1423814/;' $target_dir/$enog".fa"
-	makeblastdb -in $target_dir/$enog".fa" -dbtype prot 
+for ENOG in $(less ${ENOG_LIST}); do
+	if [ ${DATABASE_EXISTING} -eq 0 ]; then
+		cp bactNOG_raw_algs/*"."$ENOG"."* $ENOG".fa"
+		sed 's/\..*$/\t/;s/^>/\t>/;' $ENOG".fa" | tr -d '\n\-' | tr "\t" "\n" > ${TARGET_DIR}/$ENOG".fa"
+		#some taxids changed (merged) after eggnog release. sed to replace the headers (so they fit the taxonomy tree)
+		sed -i 's/>245018$/>649756/;s/>439483$/>929558/;s/>469595$/>1639133/;s/>469596$/>100884/;s/>525366$/>1423814/;' ${TARGET_DIR}/$ENOG".fa"
+	fi
+	makeblastdb -in ${TARGET_DIR}/$ENOG".fa" -dbtype prot 
 done
 
-rm -r $tmpdir
+rm -r $TMPDIR
