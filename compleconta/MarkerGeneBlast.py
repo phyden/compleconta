c15e569b7e45194113d27dbfc566b8f092543c16
Patrick Hyden <patrick_hyden@gmx.at>
2018-05-25 10:51:58 +0200

    adding commandline options, checking for existance of blast databases and create indices if necessary

--- compleconta/MarkerGeneBlast.py
+++ compleconta/MarkerGeneBlast.py
@@ -6,7 +6,9 @@ import multiprocessing
 import Annotation
 
 def prepareFiles(gc,enog_list):
-    
+    """ function which creates a temporary directory and input, output filepairs for each sequence with the info to which enog it belongs """    
+
+
     tmp_dir=tempfile.mkdtemp()
     
     outfiles=[]
@@ -34,16 +36,56 @@ def prepareFiles(gc,enog_list):
 
 
 def runBlastJob(parameter_set):
+    """ run function which is called by the multiprocessing pool """
 
-    database, inputfile, outputfile = parameter_set
 
-    os.system("blastp -db %s -query %s -out %s -outfmt '6'" % parameter_set)
+    database, inputfile, outputfile, margin = parameter_set
 
-    best_hit = readOutput(outputfile)
+    if check_database(database) == 0:
+        os.system("blastp -db %s -query %s -out %s -outfmt '6'" % parameter_set[:3])
+        best_hit = readOutput(outputfile, margin)
+        return best_hit
+
+    else:
+        return []
+
+
+def check_database(database):
+    """ checks existance of database file & creates index files in necessary """
     
-    return best_hit
 
-def readOutput(outputfile):
+    endings=["phr","pin","psq"]
+
+    try:
+        time_db=os.path.getctime(database)
+    except OSError:
+        sys.stderr.write("Error: database file not existing or inaccessable: %s\n" % database)
+        return 1
+
+    recreate=False
+    
+    for ending in endings:
+        indexfile=".".join([database,ending])
+        try:
+            if time_db > os.path.getctime(indexfile):
+                recreate=True
+                break
+        except OSError:
+            if os.path.isfile(indexfile):
+                sys.stderr.write("Error: database index existing but inaccessable: %s\n" % indexfile)
+                return 1
+            else:
+                recreate=True
+
+    if recreate==True:
+        sys.stderr.write("Info: database indices will be created for %s\n" % database)
+        os.system("makeblastdb -in %s -dbtype prot" % database)
+    
+    return 0
+
+def readOutput(outputfile, margin):
+    """ function to parse the blastp tabular output and return the best hits (with a margin from the best bitscore downwards) """
+
 
     with open(outputfile,"r") as tmpfile_handler:
         maxscore=0
@@ -55,7 +97,7 @@ def readOutput(outputfile):
             pident=float(fields[2])
 	    bitscore=float(fields[11])
             maxscore=max(maxscore,bitscore)
-            if bitscore < maxscore*0.9:
+            if bitscore < maxscore*margin:
                 break
             else:
                 tophit.append(hit)
@@ -64,13 +106,18 @@ def readOutput(outputfile):
     return tophit
 
 
-def getTaxidsFromSequences(databasepath,gc):
+def getTaxidsFromSequences(databasepath,gc, n_threads_blast=5, margin=0.9):
+    """ master function that runs above functions parallelized in a worker pool of n_threads_blast subprocesses """
+
+    n_threads_blast=max(n_threads_blast,1) #minimum number of workers: 1
+
+    margin=min(abs(margin),1.0) #margin has to be in range 0.0-1.0
 
     best_hits=[]
 
     enog_list=gc.get_profile()
 
-    pool=multiprocessing.Pool(5)
+    pool=multiprocessing.Pool(n_threads_blast)
 
     tmp_dir, outfiles, inputfiles, enog_list, seq_list = prepareFiles(gc, enog_list)
     
@@ -78,7 +125,8 @@ def getTaxidsFromSequences(databasepath,gc):
     parameter_sets=[]
     for i in range(0,len(enog_list)):
         database=databasepath+"/"+enog_list[i]+".fa"
-        parameter_sets.append((database, inputfiles[i], outfiles[i]))
+        parameter_sets.append((database, inputfiles[i], outfiles[i], margin))
+        #best_hit=runBlastJob(parameter_sets[i])
         
     best_hits=pool.map(runBlastJob,parameter_sets)
 
